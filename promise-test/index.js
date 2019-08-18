129a82e8935e289e741b94dee43cc11994c6d645
宋梦瑶 <3076117417@qq.com>
2019-08-18 20:37:19 +0800

    test async-await

--- promise-test/index.js
+++ promise-test/index.js
@@ -40,14 +40,32 @@ function getFileContent(fileName) {
   return promise
 }
 
-getFileContent('a.json').then(aData => {
-  console.log('a data---->', aData)
-  return getFileContent(aData.next)
-}).then(bData => {
-  console.log('b data---->', bData)
-  return getFileContent(bData.next)
-}).then(cData => {
-  console.log('c data---->', cData)
-})
+// getFileContent('a.json').then(aData => {
+//   console.log('a data---->', aData)
+//   return getFileContent(aData.next)
+// }).then(bData => {
+//   console.log('b data---->', bData)
+//   return getFileContent(bData.next)
+// }).then(cData => {
+//   console.log('c data---->', cData)
+// })
 
-// async await  koa2中使用
+// async await, 这两者后面都可以跟 promise 对象
+async function readFileData () {
+  // 同步写法
+  const aData = await getFileContent('a.json')
+  console.log('a data---->', aData)
+
+  const bData = await getFileContent(aData.next)
+  console.log('b data---->', bData)
+
+  const cData = await getFileContent(bData.next)
+  console.log('c data---->', cData)
+}
+readFileData()
+
+// 要点：
+// 1. await 后面可以加 promise 对象
+// 2. await 必须包裹在 async 函数里面
+// 3. async 函数执行返回的也是一个 promise 对象
+// 4. try-catch 截获 promise 中 reject 的值
